64d35e5969901a393008af24ed7278b1b15db801
M Haidar Hanif <git@mhaidarhanif.com>
2020-02-14 20:32:15 +0800

    Start to create navigation in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,41 @@
-import PropTypes from "prop-types"
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header></header>
-  // <header
-  //   style={{
-  //     background: `#333333`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //       textAlign: `center`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
+  <header
+    style={{
+      background: `#333333`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Team</li>
+        <li>Blog</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
+  </header>
 )
 
 Header.propTypes = {
