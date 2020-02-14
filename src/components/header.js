290f4d74a21c1afa63645f7c3b31891a65e50152
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-02-14 19:53:40 +0700

    feat: add navigation bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,21 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
+
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header></header>
-  // <header
-  //   style={{
-  //     background: `#333333`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //       textAlign: `center`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
+  <header className="header">
+    <div className="header__container">
+      <h1 className="header__title">High Skill Masters</h1>
+      <div className="header__menu">
+        <a href="#">Home</a>
+        <a href="#">Blog</a>
+        <a href="#">Meetups</a>
+        <a href="#">About</a>
+      </div>
+    </div>
+  </header>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Head from "react-helmet"
 
 import Header from "./header"
 import "./layout.css"
@@ -25,12 +26,17 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Rubik:400,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          maxWidth: 700,
           paddingTop: 0,
         }}
       >
